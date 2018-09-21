6333f073e681b0d60d43223a8a6c35008ef62d71
Kevin Munc <muncman@gmail.com>
2018-09-21 12:05:51 -0400

    Initial Commit

---
+++ ProvideSwiftSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProvideSwiftSample
+//
+//  Created by Kevin Munc on 09/21/18.
+//  Copyright © 2018 Method Up. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
